4a4c3f95c6d8f7152b6e810fedb06b1461973218
ivantsov-aa <pr.minsk.ivantsov@gmail.com>
2021-02-17 21:37:52 +0300

    sections model

---
+++ script.js
@@ -0,0 +1,7 @@
+const sections = [
+    { type: 'title', value: 'Hello world!' },
+    { type: 'text', value: loremText },
+    { type: 'columns', value: [loremText, loremText, loremText] }
+];
+
+let loremText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis, voluptatibus sed labore illum architecto fuga, laudantium placeat debitis at quaerat temporibus soluta a reiciendis culpa cum nulla eum saepe.';
