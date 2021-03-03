f145660369a63df0a01ff7584590378fff795cc8
Ivantsov-aa <pr.minsk.ivantsov@gmail.com>
2021-03-03 21:55:51 +0300

    last changes events

--- src/classes/app.js
+++ src/classes/app.js
@@ -4,6 +4,23 @@ export class Application {
     }
 
     render(sections) {
+        this.initializeMobileNavigation();
+        this.renderSections(sections);
+    }
+    initializeMobileNavigation() {
+        let mobileNavigation = document.createElement('i');
+        mobileNavigation.className  = "bi bi-list mobile-nav-toggle";
+
+        const onMobileNavigationClick = () => {
+            document.body.classList.toggle('mobile-nav-active');    
+            mobileNavigation.classList.toggle('bi-list');
+            mobileNavigation.classList.toggle('bi-x');
+        }
+
+        mobileNavigation.addEventListener('click',onMobileNavigationClick);
+        this.$content.prepend(mobileNavigation);
+    }
+    renderSections(sections) {
         sections.forEach(section => {
             this.$content.insertAdjacentHTML('beforeend', section.toHTML())
         })
